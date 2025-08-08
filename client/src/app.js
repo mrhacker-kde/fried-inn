ae6607a4ff52923903e83fbde6e4318d3c3f3a18
mrhacker-kde <rnjoroge303@gmail.com>
2025-08-09 01:35:05 +0300

    Update app.js
    env.port

--- client/src/app.js
+++ client/src/app.js
@@ -42,6 +42,6 @@ app.get('/logout', (req, res) => {
     res.redirect('/');
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
